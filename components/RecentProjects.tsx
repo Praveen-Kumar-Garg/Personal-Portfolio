c36babd53cc4797e3952a1141375de0d62bb2306
Praveen07garg <praveengarg9450@gmail.com>
2024-10-13 18:49:28 +0530

    error

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -11,7 +11,7 @@ const RecentProjects = () => {
             </h1>
             <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
                 {projects.map((item) => (
-                    <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+                    <div key={item.title} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
                         <PinContainer title={item.title} href={item.link}>
                             <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                                 <div
